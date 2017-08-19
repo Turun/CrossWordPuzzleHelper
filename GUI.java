4747afc97d14437b3bce5c2bc3c50b7d94e6a0db
Turun <25162108+Turun@users.noreply.github.com>
2017-08-19 18:44:51 +0200

    added new options!
    you can now search for words with undefined length.

--- GUI.java
+++ GUI.java
@@ -4,16 +4,29 @@ import java.awt.event.*;
 
 public class GUI
 {
+    static final String instructions = "<html><body>Use an underscore(\"_\") to signal a block <br> without a character or a space</body></html>";
+    
+    
     JFrame f;
-    JTextField jtf;
-    JPanel boxes;
-    JTextField[] box;
+    JPanel middle;
+    JPanel withFixedLength;
+    JPanel woutFixedLength;
+    JCheckBox fixedLength;
     JTextArea jta;
+    JTextField wordInput;
+    JTextField checkFromStartInput;
+    JTextField checkFromEndInput;
+    JCheckBox checkFromStart;
+    JCheckBox checkFromEnd;
+    
     
     Mech mech;
     
     int len;
     char[] word;
+    char[] start;
+    char[] end;
+    
     
     public static void main(String[] args){GUI g = new GUI();}
     private GUI(){
@@ -21,14 +34,154 @@ public class GUI
     }
     
     private void makeFrame(){
+        /**structure: (_____name____ = JPanel name)
+         * 
+         *  _______________main________________
+         * | _____________input_______________ |
+         * || _____________top_______________ ||
+         * |||                               |||
+         * |||   checkbox "fixed length"     |||
+         * |||_______________________________|||
+         * || ___________middle______________ ||
+         * |||                               |||
+         * |||                               ||| either:
+         * |||                               ||| "use an underscore("_") to signal a block without a character/space" ("_" = ACSII code 95; " " = ASCII code 32)
+         * |||                               |||   [textbox]
+         * |||                               |||
+         * |||                               ||| or:
+         * |||                               |||  [checkbox] check words from the beginning
+         * |||                               |||  [textbox]["..."]
+         * |||                               |||  [checkbox] check words from the end
+         * |||                               |||  ["..."][textbox]
+         * |||                               |||
+         * |||_______________________________|||
+         * || ___________bottom______________ ||
+         * |||                               |||
+         * |||    buttons (e.g. start)       |||
+         * |||_______________________________|||
+         * ||_________________________________||
+         * | ____________output_______________ |
+         * ||                                 ||
+         * ||                                 ||
+         * ||                                 ||
+         * ||                                 ||
+         * ||                                 ||
+         * ||          text output            ||
+         * ||                                 ||
+         * ||                                 ||
+         * ||                                 ||
+         * ||                                 ||
+         * ||_________________________________||
+         * |___________________________________|
+         */
+        
+        
         f = new JFrame("Crosswordpuzzle Helper");
         f.setVisible(false);
         f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         f.addWindowListener(new WindowAdapter(){public void windowClosing(WindowEvent e){exit();}});
-        f.setLayout(new BorderLayout());
+        f.setLayout(new FlowLayout());
         
         JPanel main = new JPanel();
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+        main.setBorder(BorderFactory.createEmptyBorder(2,7,7,7));
+        
+        /**container for all the inputs**/
+        JPanel input = new JPanel();
+        input.setLayout(new BoxLayout(input, BoxLayout.Y_AXIS));
+        
+        
+        
+        /**the checkbox**/
+        JPanel top = new JPanel(new FlowLayout());
+        fixedLength = new JCheckBox("fixed length", true);
+        fixedLength.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){setFixedLength(fixedLength.isSelected());}});
+        top.add(fixedLength);
+        
+        
+        
+        /**the container for swapping the two options**/
+        middle = new JPanel(new FlowLayout());
+        
+        /**the JPanel with fixedlength (options one)**/
+        withFixedLength = new JPanel();
+        withFixedLength.setLayout(new BoxLayout(withFixedLength, BoxLayout.Y_AXIS));
+        
+        /*needed to render the output low enough to fit the witout fixed panel too*/
+        JPanel spaceholder = new JPanel();
+        JLabel space = new JLabel("<html><body> <br> <br> <br></body></html>");
+        spaceholder.add(space);
+        
+        wordInput = new JTextField("exa_pl_");
+        
+        withFixedLength.add(new JLabel(instructions));
+        withFixedLength.add(Box.createRigidArea(new Dimension(5,5)));
+        withFixedLength.add(wordInput);
+        withFixedLength.add(spaceholder);
+        
+        /**the JPanel without fixed length (option two)**/
+        woutFixedLength = new JPanel(new GridLayout(4,1,5,5));
+        
+        checkFromStart = new JCheckBox("check words from the beginning"); //CheckFromStart
+        woutFixedLength.add(checkFromStart);
+        
+        JPanel upperone = new JPanel();
+        upperone.setLayout(new BoxLayout(upperone, BoxLayout.X_AXIS));
+        checkFromStartInput = new JTextField("e_am");
+        checkFromStartInput.setHorizontalAlignment(SwingConstants.RIGHT);
+        upperone.add(checkFromStartInput);
+        upperone.add(Box.createRigidArea(new Dimension(5,5)));
+        upperone.add(new JLabel("..."));
+        woutFixedLength.add(upperone);
+        
+        checkFromEnd = new JCheckBox("check words from the end");
+        woutFixedLength.add(checkFromEnd);
+        
+        JPanel lowerone = new JPanel();
+        lowerone.setLayout(new BoxLayout(lowerone, BoxLayout.X_AXIS));
+        checkFromEndInput = new JTextField("mp_e");
+        lowerone.add(new JLabel("..."));
+        lowerone.add(Box.createRigidArea(new Dimension(5,5)));
+        lowerone.add(checkFromEndInput);
+        woutFixedLength.add(lowerone);
+        
+        
+        /**contains the buttons**/
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.X_AXIS));
+        bottom.add(Box.createHorizontalGlue());
+        
+        JButton start = new JButton("Start");
+        start.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){start();}});
+        bottom.add(start);
+        
+        
+        
+        /**JPanel for all the output**/
+        
+        jta = new JTextArea();
+        jta.setEditable(false);
+        JScrollPane output = new JScrollPane(jta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        output.setMinimumSize(new Dimension(200,100));
+        output.setPreferredSize(new Dimension(300,400));
+        output.setMaximumSize(new Dimension(1000,1000));
+        
+        
+        /**combining all the JPanels in the right way**/
+        middle.add(withFixedLength);
+        
+        input.add(top);
+        input.add(middle);
+        input.add(bottom);
+        
+        main.add(input);
+        main.add(Box.createRigidArea(new Dimension(10,10)));
+        main.add(output);
+        
+        f.add(main);
+        
+        
+        /*
         JPanel controls = new JPanel(new GridLayout(2,1,5,5));
         main.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         
@@ -58,12 +211,6 @@ public class GUI
         confirmField.add(start);
         bottom.add(confirmField);
         
-        jta = new JTextArea();
-        jta.setEditable(false);
-        JScrollPane output = new JScrollPane(jta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        output.setMinimumSize(new Dimension(200,100));
-        output.setPreferredSize(new Dimension(300,400));
-        output.setMaximumSize(new Dimension(1000,1000));
         
         controls.add(top);
         controls.add(bottom);
@@ -74,6 +221,8 @@ public class GUI
         f.add(new JLabel(""), BorderLayout.EAST);
         f.add(new JLabel(""), BorderLayout.SOUTH);
         f.add(new JLabel(""), BorderLayout.WEST);
+        */
+        
         
         f.pack();
         f.setLocationRelativeTo(null);
@@ -86,59 +235,106 @@ public class GUI
         System.exit(0);
     }
     
-    private void getInputWordlength(){
-        try{
-            String text = jtf.getText().replace(" ",""); //input without spaces
-            len = Integer.valueOf(text);
-        }catch(NullPointerException e){
-            jtf.setText("Intput a number");
+    private void setFixedLength(boolean fixedLength){
+        middle.removeAll();
+        
+        if(fixedLength){
+            middle.add(withFixedLength);
+        }else{
+            middle.add(woutFixedLength);
+        }
+        
+        middle.validate();
+        middle.repaint();
+        //f.revalidate();
+        //f.repaint();
+    }
+    
+    private int getMode(){
+        int re = 0;
+        if(fixedLength.isSelected()){
+            re = mech.FITCHAR;
+        }else{
+                 if( checkFromStart.isSelected() &&  checkFromEnd.isSelected()){re = mech.FITBOTH;}
+            else if( checkFromStart.isSelected() && !checkFromEnd.isSelected()){re = mech.FITBEG;}
+            else if(!checkFromStart.isSelected() &&  checkFromEnd.isSelected()){re = mech.FITEND;}
+            else if(!checkFromStart.isSelected() && !checkFromEnd.isSelected()){re = mech.FITNOTHING;}
+        }
+        return re;
+    }
+    
+    private char[] getWord(){
+        if(fixedLength.isSelected()){
+            return modifyInput(wordInput.getText());
+        }else{
+            return null;
         }
     }
     
-    private void adjustWordlength(){
-        getInputWordlength();
-        
-        
-        word = new char[len];
-        if(box != null){
-            for(int i = 0; i<box.length && i<word.length; i++){
-                String text = box[i].getText();
-                text = text.toLowerCase();
-                text = text.replace(" ","");
-                text = text.trim();
-                
-                if(text.isEmpty()){
-                    word[i] = (char)0;
-                }else{
-                    word[i] = text.toCharArray()[0];
-                }
-            }
+    private char[] getStart(){
+        if(checkFromStart.isSelected()){
+            return modifyInput(checkFromStartInput.getText());
         }else{
-            for(int i = 0; i<word.length; i++){
-                word[i] = (char)0;
+            return null;
+        }
+    }
+    
+    private char[] getEnd(){
+        if(checkFromEnd.isSelected()){
+            return modifyInput(checkFromEndInput.getText());
+        }else{
+            return null;
+        }
+    }
+    
+    private char[] modifyInput(String text){
+        text = text.toLowerCase();
+        text = text.replace(" ", "");
+        char[] re = text.toCharArray();
+        for(int i = 0; i<re.length; i++){
+            if(re[i] == 95){
+                re[i] = 0;
             }
         }
-        
-        box = new JTextField[len];
-        boxes.removeAll();
-        
-        for(int i = 0; i<box.length; i++){
-            box[i] = new JTextField(String.valueOf(word[i]), 3);
-            boxes.add(box[i]);
-        }
-        
-        boxes.validate();
-        boxes.repaint();
+        return re;
     }
     
     private void start(){
-        adjustWordlength();
-        mech = new Mech(this, word);
+        mech = new Mech(this, getWord(), getStart(), getEnd(), getMode());
         mech.start();
     }
     
     public void print(String s){
         jta.append(s+"\n");
     }
-   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Mech.java
+++ Mech.java
@@ -2,21 +2,138 @@ import java.io.*;
 
 public class Mech extends Thread
 {
+    static final int FITBEG = 0;
+    static final int FITCHAR = 1;
+    static final int FITBOTH = 2;
+    static final int FITEND = 3;
+    static final int FITNOTHING = 666;
+    
+    
     GUI gui;
     
     String filename = "";
+    int mode;
     boolean running = true;
     char[] goal;
+    char[] start;
+    char[] end;
     
-    public Mech(GUI g, char[] word){
+    public Mech(GUI g, char[] word, char[] start, char[] end, int mode){
         goal = word;
         gui = g;
+        this.start = start;
+        this.end = end;
+        this.mode = mode;
+        
+        setFilename();
     }
     
     public void run(){
-        if(!setFilename()){return;}
+        if(filename.length() < 3){return;}
         
         print("---start---");
+        switch(mode){
+            case(FITBEG):{fitBeg();break;}
+            case(FITCHAR):{fitChar();break;}
+            case(FITBOTH):{fitBoth();break;}
+            case(FITEND):{fitEnd();break;}
+            case(FITNOTHING):{print("please select at least one checkbox");break;}
+            default:{
+                print("Error! Send the devs this: Mech - invalid mode number: "+String.valueOf(mode));
+                break;
+            }
+        }
+        print("---done---");
+    }
+    
+    private void fitBeg(){
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+            outerLoop:
+            while(running && ((line = br.readLine()) != null)){
+                //line = line.toLowerCase();
+                char[] word = line.toLowerCase().toCharArray();
+                
+                /**if the word we have is shorter than the word we search**/
+                if(word.length < start.length){continue outerLoop;} 
+                
+                /**runs through the given array and compares it with the word we have. if the char doesn't match the seach continues**/
+                innerLoop:
+                for(int i = 0; i<start.length; i++){
+                    if((word[i] != start[i]) && (start[i] != 0) && (word[i] != 32) && (word[i] != 95)){ //continue, if they are not the same, start is defined and word is not a space/underscore
+                        continue outerLoop;
+                    }
+                }
+                
+                print(line);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    private void fitEnd(){
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+            outerLoop:
+            while(running && ((line = br.readLine()) != null)){
+                //line = line.toLowerCase();
+                char[] word = line.toLowerCase().toCharArray();
+                
+                /**if the word we have is shorter than the word we search**/
+                if(word.length < end.length){continue outerLoop;}
+                
+                /**starts at the end of the endArray and goes to the front. compares every single char and when they don't match the search contines**/
+                innerLoop:
+                for(int i = end.length-1; i >= 0; i--){
+                    if((word[word.length-end.length+i] != end[i]) && (end[i] != 0) && (word[i] != 32) && (word[i] != 95)){
+                        continue outerLoop;
+                    }
+                }
+                
+                print(line);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    private void fitBoth(){
+        String line;
+        try(BufferedReader br = new BufferedReader(new FileReader(filename))){
+            outerLoop:
+            while(running && ((line = br.readLine()) != null)){
+                //line = line.toLowerCase();
+                char[] word = line.toLowerCase().toCharArray();
+                
+                /**if the word we have is too short, it immediately gets discarded**/
+                if(word.length < start.length || word.length < end.length){continue outerLoop;}
+               
+                /**the following methods are the same as in the fitEnd and fitBeg methods**/                
+                /**runs through the given array and compares it with the word we have. if the char doesn't match the seach continues**/
+                innerLoop1:
+                for(int i = 0; i<start.length; i++){
+                    if((word[i] != start[i]) && (start[i] != 0) && (word[i] != 32) && (word[i] != 95)){
+                        continue outerLoop;
+                    }
+                }
+                
+                /**starts at the end of the endArray and goes to the front. compares every single char and when they don't match the search contines**/
+                innerLoop2:
+                for(int i = end.length-1; i >= 0; i--){
+                    if((word[word.length-end.length+i] != end[i]) && (end[i] != 0) && (word[i] != 32) && (word[i] != 95)){
+                        continue outerLoop;
+                    }
+                }
+                
+                print(line);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    private void fitChar(){
         String line;
         try(BufferedReader br = new BufferedReader(new FileReader(filename))){
             outerLoop:
@@ -28,29 +145,25 @@ public class Mech extends Thread
                 char[] word = line.toCharArray();
                 innerLoop:
                 for(int i = 0; i<goal.length; i++){
-                    if((word[i] != goal[i]) && (goal[i] != 0)){
+                    if((word[i] != goal[i]) && (goal[i] != 0) && (word[i] != 32) && (word[i] != 95)){
                         continue outerLoop;
                     }
                 }
                 
                 print(line);
             }
-            print("---done---");
         }catch(IOException e){
             e.printStackTrace();
         }
     }
     
-    private boolean setFilename(){
-        boolean success = false;
+    private void setFilename(){
         try(BufferedReader br = new BufferedReader(new FileReader("./DictionaryLocation.txt"))){
             filename = br.readLine();
-            success = true;
         }catch(IOException e){
             print("\n\n Failed to read the location of the Dictionary file.\n");
-            print("Make sure you have a file called \n \"DictionaryLocation.txt\" in the directory of this File. \n That File must contain the absolute path of your \n dictionary file. Make sure to include the .txt ending \n and to escape every \"\\\" by putting a  \"\\\" in front of it.");
+            print("Make sure you have a file called \n \"DictionaryLocation.txt\" in the directory of this File. \n That File must contain the absolute path of your \n dictionary file. Make sure to include the .txt ending.");
         }
-        return success;
     }
     
     private void print(String line){
